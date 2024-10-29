c8e82df8722e354c991bc1b6b0e7a25d84c17ffc
BCS-ouvrage-ia <bcs.ouvrage.ia@gmail.com>
2024-10-29 17:49:13 +0100

    Delete test_webhook2.sh

--- test_webhook2.sh
+++
@@ -1,6 +0,0 @@
-curl -X POST \
-   https://5011-2a02-842a-5d00-8601-cc62-a06f-8d43-2ea6.ngrok-free.app/webhook-test/enregistrer-memoire-technique \
-  -F "nom entreprise=Entreprise XYZ" \
-  -F "prénom=Jean" \
-  -F "nom=Dupont" \
-  -F "file=@docs_test/memoire-technique.pdf"
